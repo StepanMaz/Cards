491f61c208c4b51ae2fc5c2cf75c71e770ef7a12
Stepan Maznyi <smaznyi@gmail.com>
2023-10-25 00:20:02 +0300

    remove additional types folder

--- apps/api/src/types/auth/request.ts
+++
@@ -1,4 +0,0 @@
-import type { Request as ExpressRequest } from "express";
-import { HttpContextUser } from ".";
-
-export type AuthorizedRequest = ExpressRequest & { user: HttpContextUser };
--- apps/api/src/types/auth/user.ts
+++
@@ -1,3 +0,0 @@
-import type { JwtStrategy } from "src/services/auth";
-
-export type HttpContextUser = Awaited<ReturnType<JwtStrategy["validate"]>>;
